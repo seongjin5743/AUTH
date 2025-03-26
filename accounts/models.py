e99cd6030ba20c661fb16a899788faa5ecc07db1
seongjin <seongjin5743@naver.com>
2025-03-26 10:33:33 +0900

    user modeling / migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser): # settings.py에 AUTH_USER_MODEL = '앱이름.User'를 추가해준다.
+    pass
