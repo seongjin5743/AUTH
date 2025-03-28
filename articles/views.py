c94ce5007935912693cd0b81396d1fc066528e1f
seongjin <seongjin5743@naver.com>
2025-03-28 11:49:09 +0900

    article update

--- articles/views.py
+++ articles/views.py
@@ -49,7 +49,24 @@ def delete(request, id):
 
 @login_required
 def update(request, id):
-    pass
+    article = Article.objects.get(id=id)
+
+    if request.user != article.user:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', id=id)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+    }
+    
+    return render(request, 'update.html', context)
+
 
 @login_required
 def comment_create(request, article_id):
